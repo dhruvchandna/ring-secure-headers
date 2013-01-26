9cb388922dfdde012434d46285a4b6304080d701
Dhruv Chandna <dhruvchandna@gmail.com>
2013-01-26 11:25:28 +0530

    added test when hsts header is already set in response

--- test/compojure_secure_headers/core_test.clj
+++ test/compojure_secure_headers/core_test.clj
@@ -5,5 +5,10 @@
 (deftest wrap-hsts-header-test
   (testing "response without hsts header"
     (let [handler (wrap-hsts-header (constantly {}))]
-      (is (= (handler {}) {:headers {"Strict-Transport-Security" "max-age=31536000;"}})))))
+      (is (= (handler {}) {:headers {"Strict-Transport-Security" "max-age=31536000;"}}))))
+  
+  (testing "response with hsts header already set"
+    (let [handler (wrap-hsts-header
+                   (constantly {:headers {"Strict-Transport-Security" "max-age=0;"}}))]
+      (is (= (handler {}) {:headers {"Strict-Transport-Security" "max-age=0;"}})))))
 
