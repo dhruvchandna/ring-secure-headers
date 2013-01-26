3f72720a166f5da7dabd5de43dd732c97f9817c4
Dhruv Chandna <dhruvchandna@gmail.com>
2013-01-26 12:30:52 +0530

    added more tests

--- src/compojure_secure_headers/core.clj
+++ src/compojure_secure_headers/core.clj
@@ -1,12 +1,23 @@
 (ns compojure-secure-headers.core
   (:use ring.util.response))
 
+(defn- create-hsts-header
+  [& [max-age include-subdomains]]
+  (str
+   "max-age="
+   (if max-age max-age "31536000") ";"
+   (if include-subdomains "includeSubDomains")))
+
 (defn wrap-hsts-header
   "Add the 'Strict-Transport-Security' header
    response."
-  [handler]
-  (fn [req]
+  [handler & [options]]
+  (let [opts (if options options {:max-age 31536000 :include-subdomains false})]
+   (fn [req]
     (if-let [resp (handler req)]
       (if (get-in resp [:headers "Strict-Transport-Security"])
         resp
-        (header resp "Strict-Transport-Security" "max-age=31536000;")))))
+        (header resp "Strict-Transport-Security"
+                (create-hsts-header (opts :max-age) (opts :include-subdomains))))))))
+
+
--- test/compojure_secure_headers/core_test.clj
+++ test/compojure_secure_headers/core_test.clj
@@ -10,5 +10,26 @@
   (testing "response with hsts header already set"
     (let [handler (wrap-hsts-header
                    (constantly {:headers {"Strict-Transport-Security" "max-age=0;"}}))]
-      (is (= (handler {}) {:headers {"Strict-Transport-Security" "max-age=0;"}})))))
+      (is (= (handler {}) {:headers {"Strict-Transport-Security" "max-age=0;"}}))))
+
+  (testing "response with custom max-age"
+    (let [handler (wrap-hsts-header
+                   (constantly {}) {:max-age 1000})]
+      (is (= (handler {}) {:headers {"Strict-Transport-Security" "max-age=1000;"}}))))  
+
+  (testing "response with default max-age and custom domain inclusion"
+    (let [handler (wrap-hsts-header
+                   (constantly {}) {:include-subdomains true})]
+      (is (= (handler {})
+             {:headers {"Strict-Transport-Security" "max-age=31536000;includeSubDomains"}}))))  
+
+  (testing "response with custom max-age and custom domain inclusion"
+    (let [handler (wrap-hsts-header
+                   (constantly {}) {:max-age 2000 :include-subdomains true})]
+      (is (= (handler {})
+             {:headers {"Strict-Transport-Security" "max-age=2000;includeSubDomains"}}))))
+  
+  (testing "nil response"
+    (let [handler (wrap-hsts-header (constantly nil))]
+      (is (nil? (handler {}))))))
 
