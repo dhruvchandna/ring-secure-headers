6cc64efd26fd19568cd317a0d79adf35c72a0c60
Dhruv Chandna <dhruvchandna@gmail.com>
2013-02-02 12:48:43 +0530

    changed project description

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject ring-secure-headers "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Ring Middleware library to add secure headers to a HTTP response"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
