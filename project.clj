7817ebd21aaf9267ee1c856d59da31f830e6738a
Dhruv Chandna <Dhruv_Chandna@intuit.com>
2014-03-26 23:40:54 +0530

    fixed license and updated clojure versions

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject ring-secure-headers "0.1.0-SNAPSHOT"
+(defproject ring-secure-headers "0.1.0"
   :description "Ring Middleware library to add secure headers to a HTTP response"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [ring/ring-core "1.1.7"]])
+  :url "https://github.com/dhruvchandna/ring-secure-headers"
+  :license {:name "MIT"
+            :url ""}
+  :dependencies [[org.clojure/clojure "1.5.0"]
+                 [ring/ring-core "1.2.2"]])
