3fe3e0e80e943d4775b5e277cbe797d32096825a
Dhruv Chandna <dhruvchandna@gmail.com>
2014-04-10 00:40:37 +0530

    Bumped up the version to 0.2.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject dhruv/ring-secure-headers "0.1.0"
+(defproject dhruv/ring-secure-headers "0.2.0"
   :description "Ring Middleware library to add secure headers to a HTTP response"
   :url "https://github.com/dhruvchandna/ring-secure-headers"
   :license {:name "MIT"
